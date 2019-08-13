61abbbabfc44b94edd3b4dd4056599503ebc8fd4
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-08-12 18:44:27 -0600

    test_verse_1 developed

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,9 +4,19 @@ class Bottles
   # You can pass bottle number
   #
   def verse(bottle)
-    "#{bottle} bottles of beer on the wall, #{bottle} bottles of beer." +
-        "\nTake one down and pass it around, #{bottle - 1} bottle#{"s" if bottle - 1 > 1} of beer on the wall." +
+    "#{bottle} bottle#{"s" if bottle > 1} of beer on the wall, #{bottle} bottle#{"s" if bottle > 1} of beer." +
+        "\nTake #{bottle == 1? "it" : "one"} down and pass it around, " + last_part(bottle) +
         "\n"
+  end
+
+  private
+  def last_part(bottle)
+    if bottle == 1
+      "no more bottles of beer on the wall."
+    else
+      "#{bottle - 1} bottle#{"s" if bottle - 1 > 1} of beer on the wall."
+    end
+
   end
 end
 
